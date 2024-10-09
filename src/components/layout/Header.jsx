283c664fc5293ead7ba0c58ed4771a5ab49bd7ac
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2024-10-10 00:17:13 +0530

    alter in logo

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
             animate="animate"
     className={`bg-white/30 bg-opacity-80 flex fixed transition-shadow duration-300 ease-in-out ${scroll ? 'shadow-md' : ''} justify-between items-center backdrop-blur top-0 left-0 right-0 z-10 h-32`}>
           <div className="flex items-center">
-          <img src={logo} alt='logo' className='w-96 h-48 cursor-pointer' />
+          <img src={logo} alt='logo' className='w-[400px] h-40 cursor-pointer' />
           </div>
               <div className=" text-xl mx-14 hidden md:block p-2 ">
                  <ul className="flex flex-wrap flex-grow gap-8 items-center hover:text-primary">
