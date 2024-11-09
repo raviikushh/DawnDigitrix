4b2f76782e4121e3c310ed2be5184f93479ae690
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2024-11-10 00:02:59 +0530

    services page

--- src/components/services/ServicePage.jsx
+++ src/components/services/ServicePage.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const ServicePage = () => {
   return (
-    <div className="container mx-auto px-4 py-12 mt-20">
+    <div className="container mx-auto px-4 py-12">
       {/* Services Section */}
       <div className="container mx-auto px-4 py-12 mt-20">
         <h1 className="text-4xl font-bold mb-6 text-center text-gray-900">Our Services</h1>
